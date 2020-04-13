eaf090fc5353f7242f45021f3fa13050456d4790
Vincent <vincent.bechmann@gmail.com>
2020-04-13 09:55:30 +0200

    Update main.js

--- snek/main.js
+++ snek/main.js
@@ -141,6 +141,7 @@ function initGame(){
         else if(e.code==="ArrowRight") snek.direction = 1;
         else if(e.code==="ArrowDown")  snek.direction = 2;
         else if(e.code==="ArrowLeft")  snek.direction = 3;
+        e.stopPropagation
     });
 
     var startx = 0;
